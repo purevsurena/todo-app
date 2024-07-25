9594e3793f245eb59c7dd73e62ab3c14dbb83287
purevsurena <a.purevsuren0120@gmail.com>
2024-07-25 22:52:38 +0700

    feat: Install and configure Prettier with recommended plugin and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "eslint:recommended"],
+  extends: ["expo", "eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
