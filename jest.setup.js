df7fc7d5fa8543bf48e7e7cbc353570bda7e4512
purevsurena <a.purevsuren0120@gmail.com>
2024-07-28 17:07:23 +0700

    test: Mock useNavigation hook and expo-router for consistent testing

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,28 @@ import "@testing-library/jest-native/extend-expect";
 jest.mock('@freakycoder/react-native-bounceable', () => {
   return jest.fn().mockImplementation(({ children }) => children);
 });
+
+
+// Mock the useNavigation hook
+jest.mock("@react-navigation/native", () => {
+  const actualNav = jest.requireActual("@react-navigation/native");
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      ...jest.fn(),
+      goBack: jest.fn(),
+    }),
+  };
+});
+
+// Mock the expo-router
+jest.mock("expo-router", () => {
+  const actualRouter = jest.requireActual("expo-router");
+  return {
+    ...actualRouter,
+    useNavigation: () => ({
+      ...jest.fn(),
+      goBack: jest.fn(),
+    }),
+  };
+});
