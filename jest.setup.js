24be52a16dcced60938b13293993728e3a0a6308
purevsurena <a.purevsuren0120@gmail.com>
2024-07-30 03:14:43 +0700

    test: Mock AsyncStorage for consistent testing

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,10 @@
 import "@testing-library/jest-native/extend-expect";
 
-jest.mock('@freakycoder/react-native-bounceable', () => {
+jest.mock("@freakycoder/react-native-bounceable", () => {
   return jest.fn().mockImplementation(({ children }) => children);
 });
 
+jest.mock("@react-native-async-storage/async-storage", () => {});
 
 // Mock the useNavigation hook
 jest.mock("@react-navigation/native", () => {
